a64e7173b53b6bbb6177f538a9910fd2fc6c241b
Alexandre Menezes <amenezes031@gmail.com>
2021-04-20 23:51:52 -0300

    Create page using the concept of SPA

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
+// SPA Single Page Application
+
+import { useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
+
   return (
     <h1>Home</h1>
   );
