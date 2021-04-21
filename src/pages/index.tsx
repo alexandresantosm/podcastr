b239d120f3317fecdfed76541592c247e1284c61
Alexandre Menezes <amenezes031@gmail.com>
2021-04-21 09:44:45 -0300

    Add axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,18 @@
-// SSG Static Site Generation
+import { GetStaticProps } from 'next';
+import { api } from '../services/api';
 
-export default function Home(props) {
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  // ...
+}
+
+type HomeProps = {
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Home</h1>
@@ -9,9 +21,14 @@ export default function Home(props) {
   );
 }
 
-export async function getStaticProps() {
-  const response = await fetch('http://localhost:3333/episodes');
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    }
+  });
 
   return {
     props: {
