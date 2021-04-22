0a259ef53cbb4dfe143b7f5e245b58fcb8597c58
Alexandre Santos <amenezes031@gmail.com>
2021-04-22 14:38:51 -0300

    Dinamic static page generation

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -52,7 +52,7 @@ export default function Episode({ episode }: EpisodeProps) {
         <span>{episode.durationAsString}</span>
       </header>
 
-      <div className={styles.description} 
+      <div className={styles.description}
         dangerouslySetInnerHTML={{ __html: episode.description }}
       />
     </div>
@@ -60,8 +60,24 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await api.get('episodes', {
+    params: {
+      _limit: 2,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const paths = data.map(episode => {
+    return {
+      params: {
+        slug: episode.id
+      }
+    }
+  });
+
   return {
-    paths: [],
+    paths,
     fallback: 'blocking'
   }
 }
