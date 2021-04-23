56ded4404debec1e418fd5722cdd62f0c6ff4a77
Alexandre Santos <amenezes031@gmail.com>
2021-04-23 14:26:28 -0300

    Control shuffle

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,12 +13,14 @@ type PlayerContextData = {
   currentEpisodeIndex: number;
   isPlaying: boolean;
   isLooping: boolean;
+  isShuffling: boolean
   play: (episode: Episode) => void;
   playNext: () => void;
   playPrevious: () => void;
   playList: (list: Episode[], index: number) => void;
   togglePlay: () => void;
   toggleLoop: () => void;
+  toggleShuffle: () => void;
   setPlayingState: (state: boolean) => void;
   hasNext: boolean;
   hasPrevious: boolean;
@@ -35,6 +37,7 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const [isLooping, setIsLooping] = useState(false);
+  const [isShuffling, setIsShuffling] = useState(false);
 
   function play(episode: Episode) {
     setEpisodeList([episode]);
@@ -56,6 +59,10 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
     setIsLooping(!isLooping);
   }
 
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling);
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state);
   }
@@ -64,7 +71,11 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
   const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
 
   function playNext() {
-    if (hasNext) {
+    if (isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length);
+
+      setCurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else if (hasNext) {
       setCurrentEpisodeIndex(currentEpisodeIndex + 1);
     }
   }
@@ -84,12 +95,14 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
       playNext,
       playPrevious,
       isPlaying,
+      isLooping,
+      isShuffling,
       togglePlay,
+      toggleLoop,
+      toggleShuffle,
       setPlayingState,
       hasPrevious,
       hasNext,
-      isLooping,
-      toggleLoop
       }}
     >
       {children}
